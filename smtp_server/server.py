749e9f9fdd0ca600aaad5582233b1421c3f5ab84
Pa04rth <parth.22440@knit.ac.in>
2025-07-28 23:27:06 +0530

    Changes in main

--- smtp_server/server.py
+++ smtp_server/server.py
@@ -1,7 +1,7 @@
-# Responsible for starting SMTP server
 from aiosmtpd.controller import Controller
-from .handler import DBHandler
+from smtp_server.handler import DBHandler
 from config import settings
+
 if __name__ == '__main__':
     handler = DBHandler()
     controller = Controller(handler, hostname='0.0.0.0', port=settings.SMTP_PORT)
